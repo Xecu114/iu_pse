68f4a0c3e877363060e0ed612e4a41dda969788a
Xecu114 <jonashuber1999@gmail.com>
2024-12-23 16:05:15 +0100

    feat(constants): add pastel colours

--- common/constants.py
+++ common/constants.py
@@ -17,6 +17,13 @@ BLUE = (0, 0, 255)
 GREY = (128, 128, 128)
 GREEN = (0, 200, 0)
 GREEN_LIGHTER = (0, 255, 50)
+PASTEL_BEIGE_HEX = '#f7ede3'
+PASTEL_OCEANBAY_HEX = '#648181'
+PASTEL_OCEANBAY_RGB = (100, 129, 129)
+PASTEL_ROSE_HEX = '#f5cac2'
+PASTEL_RED_HEX = '#f28582'
+PASTEL_SOFTCORAL_HEX = '#edc3ae'
+PASTEL_TURQUOISE_HEX = '#c1e7e3'
 
 assets_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "assets")
 
