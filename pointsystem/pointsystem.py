41b14645dd436cf1c015f5323777e0b5931f3d11
Xecu114 <jonashuber1999@gmail.com>
2024-12-29 13:15:53 +0100

    feat: save and load data
    now saving and loading:
    "total_points", "available_points", "pomodoro_work_input", "pomodoro_break_input", "timer_input_field", "text_box"

--- pointsystem/pointsystem.py
+++ pointsystem/pointsystem.py
@@ -1,38 +1,38 @@
-import json
-import os
 
 
 class PointSystem:
     def __init__(self):
         self.total_points = 0
         self.available_points = 0
-        self.filename = "data_pointsystem.json"
-        self.load_data()
+        # self.filename = "data_pointsystem.json"
+        # self.load_data()
 
     def add_points(self, points: int):
         """Add points to the total and available points."""
         self.total_points += points
         self.available_points += points
-        self.save_data()
     
     def remove_points(self, points: int):
         """Remove points from the available points."""
         self.available_points -= points
-        self.save_data()
+
+    def set_points(self, total_points, available_points):
+        self.total_points = total_points
+        self.available_points = available_points
 
     def get_points(self):
         """Return the current points."""
         return self.total_points, self.available_points
     
-    def save_data(self):
-        data = {"total_points": self.total_points, "available_points": self.available_points}
-        with open(self.filename, "w") as file:
-            json.dump(data, file)
+    # def save_data(self):
+    #     data = {"total_points": self.total_points, "available_points": self.available_points}
+    #     with open(self.filename, "w") as file:
+    #         json.dump(data, file)
     
-    def load_data(self):
-        if not os.path.exists(self.filename):
-            self.save_data()
-        with open(self.filename, "r") as file:
-            data = json.load(file)
-        self.total_points = data["total_points"]
-        self.available_points = data["available_points"]
+    # def load_data(self):
+    #     if not os.path.exists(self.filename):
+    #         self.save_data()
+    #     with open(self.filename, "r") as file:
+    #         data = json.load(file)
+    #     self.total_points = data["total_points"]
+    #     self.available_points = data["available_points"]
