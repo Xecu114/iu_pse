72708e168635849bd1f155329c169fb55c2f45ac
Xecu114 <jonashuber1999@gmail.com>
2024-12-29 13:56:44 +0100

    refactor: now called "session" and "productivitysession"

--- main_pw.py
+++ main_pw.py
@@ -1,11 +1,11 @@
 import sys
 from PyQt6.QtWidgets import QApplication
-from productivitywindow.session import MainSession
+from productivitysession.session import MainSession
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    # Create Main Window with all functionalities regarding productivity
+    # Create MainSession with all functionalities regarding productivity
     # like timemanagement, pointsystem, projectmanagement
-    window = MainSession()
-    window.show()
+    session = MainSession()
+    session.show()
     sys.exit(app.exec())
