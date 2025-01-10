7ae39cffc1961835e168b34fe0be28268b2a9d4e
Xecu114 <jonashuber1999@gmail.com>
2025-01-10 23:39:25 +0100

    quick backup before massiv changes

--- virtualgarden/gardenobjects.py
+++ virtualgarden/gardenobjects.py
@@ -1,29 +1,6 @@
 import pygame
 from common.constants import SQUARE_SIZE
 
-placed_objects = []
-loaded_images = {}
 
 
-class GardenObject:
-    def __init__(self, name, image):
-        self.name = name
-        if image in loaded_images:
-            self.image = loaded_images[image]
-        else:
-            self.image = pygame.transform.scale(pygame.image.load(image),
-                                                (SQUARE_SIZE, SQUARE_SIZE))
-            loaded_images[image] = self.image
 
-
-class PlacedObject:
-    def __init__(self, garden_object, location):
-        self.garden_object = garden_object
-        self.location = location
-        placed_objects.append(self)
-
-    def delete(self):
-        placed_objects.remove(self)
-        
-    def draw(self, win):
-        win.blit(self.garden_object.image, self.location)
